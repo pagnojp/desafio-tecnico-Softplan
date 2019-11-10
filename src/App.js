04abfd874217594500714ff01b8dd43714204781
João Pedro Pagno <pagno.jp@gmail.com>
2019-11-10 15:11:17 -0300

    add main page

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import React from 'react';
 import GlobalStyle from './styles/global';
+import Main from './pages/Main';
 
 const App = () => (
   <>
     <GlobalStyle />
+    <Main />
   </>
 );
 
---
+++ src/pages/Main/index.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import { Container } from './styles';
+import Header from '../../components/Header';
+
+const Main = () => (
+  <>
+    <Container>
+      <Header />
+    </Container>
+  </>
+);
+
+export default Main;
