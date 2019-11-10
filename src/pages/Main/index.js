db5869d8b53889fc1d985cf2045b4c43b152a5b3
João Pedro Pagno <pagno.jp@gmail.com>
2019-11-10 17:42:10 -0300

    add Form

--- src/pages/Main/index.js
+++ src/pages/Main/index.js
@@ -1,11 +1,18 @@
 import React from 'react';
-import { Container } from './styles';
+import { Container, Form } from './styles';
 import Header from '../../components/Header';
 
 const Main = () => (
   <>
     <Container>
       <Header />
+      <Form>
+        <input
+          type="text"
+          placeholder="Pesquisar"
+        />
+        <button type="submit">OK</button>
+      </Form>
     </Container>
   </>
 );
