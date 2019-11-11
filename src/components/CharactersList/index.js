da3e06ef55f94547d4c3b41b3fab6a36df43d498
João Pedro Pagno <pagno.jp@gmail.com>
2019-11-11 09:04:13 -0300

    add card style

--- src/components/CharactersList/index.js
+++ src/components/CharactersList/index.js
@@ -10,6 +10,18 @@ const CharactersList = () => (
         <h2>3-D MAN</h2>
       </Card>
     </Link>
+    <Link to="/character/1">
+      <Card>
+        <img src="https://terrigen-cdn-dev.marvel.com/content/prod/1x/3dman442.jpg" alt="3d" />
+        <h2>ABDUL ALHAZRED</h2>
+      </Card>
+    </Link>
+    <Link to="/character/1">
+      <Card>
+        <img src="https://terrigen-cdn-dev.marvel.com/content/prod/1x/3dman442.jpg" alt="3d" />
+        <h2>ALPHA THE ULTIMATE MUTANT</h2>
+      </Card>
+    </Link>
   </Container>
 );
 
--- src/components/CharactersList/styles.js
+++ src/components/CharactersList/styles.js
@@ -6,24 +6,32 @@ export const Container = styled.div`
   justify-content: center;
   flex-wrap: wrap;
   margin-top: 40px;
+  a {
+    text-decoration: none;
+  }
 `;
 
 export const Card = styled.div`
-  width: 190px;
   background-color: #333;
   border-radius: 3px;
   display: flex;
   flex-direction: column;
+  height: 330px;
   margin: 10px 10px;
+  width: 210px;
+
+  &:hover {
+    background-color: #444;
+  }
   img {
+    border-radius: 3px;
+    margin: 10px 10px 0 10px;
     width: 190px;
+
   }
-  strong {
-    font-size: 24px;
-    margin-top: 10px;
-  }
-  small {
-    font-size: 14px;
-    color: #666666;
+  h2 {
+    color: #FFF;
+    font-size: 20px;
+    padding: 20px;
   }
 `;
