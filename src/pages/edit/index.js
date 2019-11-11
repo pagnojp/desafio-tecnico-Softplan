b77418057d0270f4f0f86ddc8b94dbf3642054f8
João Pedro Pagno <pagno.jp@gmail.com>
2019-11-11 11:03:36 -0300

    add edit character page

--- src/pages/edit/index.js
+++ src/pages/edit/index.js
@@ -1,7 +1,23 @@
 import React from 'react';
+import Header from '../../components/Header';
+import { Container, FormContainer, Form } from './styles';
 
 const EditCharacter = () => (
-  <h2>edit</h2>
+  <>
+    <Container>
+      <Header />
+      <FormContainer>
+        <img src="https://terrigen-cdn-dev.marvel.com/content/prod/1x/3dman442.jpg" alt="3d" />
+        <Form>
+          <input
+            type="text"
+            placeholder="3D-MAN"
+          />
+          <button type="submit">Editar</button>
+        </Form>
+      </FormContainer>
+    </Container>
+  </>
 );
 
 export default EditCharacter;
