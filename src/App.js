89bd0aa291b90973694b8c0b204c3ea3bf4157f0
João Pedro Pagno <pagno.jp@gmail.com>
2019-11-11 14:52:28 -0300

    add apollo client

--- src/App.js
+++ src/App.js
@@ -1,15 +1,22 @@
 import React from 'react';
 import { BrowserRouter } from 'react-router-dom';
+import ApolloClient from 'apollo-boost';
+import { ApolloProvider } from '@apollo/react-hooks';
 import GlobalStyle from './styles/global';
-
 import Routes from './routes';
+
+const client = new ApolloClient({
+  uri: 'https://api.marvelql.com/',
+});
 
 const App = () => (
   <>
-    <GlobalStyle />
-    <BrowserRouter basename={process.env.PUBLIC_URL}>
-      <Routes />
-    </BrowserRouter>
+    <ApolloProvider client={client}>
+      <GlobalStyle />
+      <BrowserRouter basename={process.env.PUBLIC_URL}>
+        <Routes />
+      </BrowserRouter>
+    </ApolloProvider>
   </>
 );
 
