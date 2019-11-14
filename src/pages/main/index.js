82faa6e9a21a4e7186508d73074fec75e8cac54c
João Pedro Pagno <pagno.jp@gmail.com>
2019-11-13 22:54:52 -0300

    add search

--- src/pages/main/index.js
+++ src/pages/main/index.js
@@ -1,46 +1,99 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import { Link } from 'react-router-dom';
+
+import { gql } from 'apollo-boost';
+import { useQuery } from '@apollo/react-hooks';
+import Header from '../../components/Header';
 
 import {
-  Container, Form, ListContainer, CardLocal,
+  Container, Form, ListContainer, CardLocal, Card,
 } from './styles';
-import Header from '../../components/Header';
-import CharactersList from '../../components/CharactersList';
 
-// Mostrar salvos no localstorage
+// Verificar e Mostrar salvos no localstorage
 const local = JSON.parse(
   window.localStorage.getItem('@softplanMarvel'),
 );
 if (!local) {
   window.localStorage.setItem('@softplanMarvel', '[]');
 }
+
 function LocalCharacters() {
   const localCharacters = JSON.parse(localStorage.getItem('@softplanMarvel'));
+  // const [list, updateList] = useState(localCharacters);
+  // const handleRemoveItem = ({ id }) => {
+  //   updateList(list.filter((item) => item.id.id !== id));
+  //   window.localStorage.setItem('@softplanMarvel', JSON.stringify(list));
+  // };
+
   return localCharacters.map(({ id, name }) => (
     <CardLocal key={Math.random() + id}>
       <img src="http://i.annihil.us/u/prod/marvel/i/mg/b/40/image_not_available.jpg" alt={name} />
       <h2>{name}</h2>
-      {/* <button type="button" onClick={() => {}}>Remover</button> */}
+      {/* <button type="button" onClick={() => handleRemoveItem(id)}>Remover</button> */}
     </CardLocal>
   ));
 }
 
-const Main = () => (
-  <>
-    <Container>
-      <Header />
-      <Form>
-        <input
-          type="text"
-          placeholder="Pesquisar"
-        />
-        <button type="submit">OK</button>
-      </Form>
-      <ListContainer>
-        <LocalCharacters />
-        <CharactersList />
-      </ListContainer>
-    </Container>
-  </>
-);
+export default function Main() {
+  const [filter, setFilter] = useState([]);
+  const [showLocal, setshowLocal] = useState(true);
+  const CHARACTERS_LIST = gql`
+    {
+      characters {
+        id
+        name
+        thumbnail
+      }
+    }`;
 
-export default Main;
+  const { loading, error, data } = useQuery(CHARACTERS_LIST);
+
+  useEffect(() => {
+    if (data) {
+      setFilter(data.characters);
+    }
+  }, [data]);
+
+  function handleChange(event) {
+    setshowLocal(false);
+    setFilter(data.characters.filter((char) => {
+      const { name } = char;
+      return name.toLowerCase().search(
+        event.target.value.toLowerCase(),
+      ) !== -1;
+    }));
+  }
+
+  return (
+    <>
+      <Container>
+        <Header />
+        <Form>
+          <input
+            type="search"
+            onChange={handleChange}
+            placeholder="Pesquisar"
+          />
+          <button type="submit">OK</button>
+        </Form>
+        { error ? (<Container>erro :( </Container>) : ('') }
+        { loading ? (<Container>loading</Container>) : (
+          <>
+            <ListContainer>
+              { showLocal ? (<LocalCharacters />) : ('') }
+              {/* <LocalCharacters /> */}
+              {filter.map((char) => (
+                <Link to={`/character/${char.id}`} key={char.id}>
+                  <Card>
+                    <img src={char.thumbnail} alt={char.name} />
+                    <h2>{char.name}</h2>
+                  </Card>
+                </Link>
+              ))}
+            </ListContainer>
+          </>
+        )}
+      </Container>
+    </>
+  );
+}
