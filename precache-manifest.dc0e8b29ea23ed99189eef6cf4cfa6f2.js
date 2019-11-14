eb2812593abb82d2b1a65dc07c43b254941074af
João Pedro Pagno <pagno.jp@gmail.com>
2019-11-14 16:08:46 -0300

    Updates

---
+++ precache-manifest.dc0e8b29ea23ed99189eef6cf4cfa6f2.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0e70a6e99730bbb0df29688957dc1644",
+    "url": "/index.html"
+  },
+  {
+    "revision": "1e3f9242008bb1b0e625",
+    "url": "/static/js/2.c134fcf5.chunk.js"
+  },
+  {
+    "revision": "53232a25dc7b7e260a15",
+    "url": "/static/js/main.94d6339c.chunk.js"
+  },
+  {
+    "revision": "47d723cd96d7bbbde53a",
+    "url": "/static/js/runtime-main.b84ad9fa.js"
+  }
+]);
